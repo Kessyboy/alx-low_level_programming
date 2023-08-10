b0e3df9ed091a119254e21cd331dc210feccb6d8
Kessyboy <Kesiena.darlington@yahoo.com>
2023-08-10 11:17:29 -0700

    hello world C programming

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 /**
-* main - display output using printf
-*
-* Return: Always 0 (Success)
-*
-*/
+ *main - display output using printf
+ *
+ * Return: Always 0 (Success)
+ *
+ */
 int main(void)
 {
 	Printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0):
+	return (0);
 }
 
