f5efeb96774f6b4afd2294dda130009d16f70ec9
Kessyboy <Kesiena.darlington@yahoo.com>
2023-08-18 09:14:02 -0700

    multiples

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+* mul - function that prints multiples of two integers
+* @a: parameter1
+* @b: parameter 2
+* Return: always 0
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
