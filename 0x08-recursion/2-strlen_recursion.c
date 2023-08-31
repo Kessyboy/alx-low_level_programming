51107597e8aa464e0360d026cd6e6f4962eabb03
Kessyboy <Kesiena.darlington@yahoo.com>
2023-08-30 22:08:41 -0700

    dreams fee real

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * _strlen_recursion - returns length of a string
- * @s: string to be measured
- * Return:length of a string
- */
+* _strlen_recursion - returns length of a string
+* @s: string to be measured
+* Return:length of a string
+*/
 
- int _strlen_recursion(char *s)
- {
- 	int longit = 0;
+int _strlen_recursion(char *s)
+{
+	int longit = 0;
 
 	if (*s)
 	{
@@ -16,4 +16,4 @@
 		longit += _strlen_recursion(s + 1);
 	}
 	return (longit);
- }
+}
