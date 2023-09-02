467a5fc52b3ef040193ac4debff330a42e4f4c37
Kessyboy <Kesiena.darlington@yahoo.com>
2023-09-01 18:02:27 -0700

    neither

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+
+/**
+* main - prints a string
+* @argc: lengths of arguments
+* @argv: pointer to an array of stringd/arguments
+* Return: always 0
+*/
+
+int main (int argc, char *argv[])
+{
+	int i;
+	int mul = 1;
+
+	if (argc < 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		mul *= atoi(argv[i]);
+	}
+	printf("%d\n", mul);
+	return (0);
+}
+
