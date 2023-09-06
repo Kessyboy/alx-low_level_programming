207c47aa80864b77dd6eae20cd51e986f1e93236
Kessyboy <Kesiena.darlington@yahoo.com>
2023-09-05 20:43:00 -0700

    float like

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+* create_array - creates array
+* @size: size of array
+* @c: character to store in array
+* Return: pointer to the array
+*/
+
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *ary;
+
+	if (size == 0)
+		return (NULL);
+
+	ary = malloc(sizeof(char) * size);
+
+	if (ary == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		ary[i] = c;
+	return (ary);
+}
