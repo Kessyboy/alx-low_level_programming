05c861e6a3ebbfe9b052513a87d1eed8aecefe8b
Kessyboy <Kesiena.darlington@yahoo.com>
2023-09-26 14:37:57 -0700

    free list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_list -function parameter
+ * @head: parameter
+ * Return:(0)Always
+ */
+
+void free_list(list_t *head)
+{
+	list_t *prep;
+
+	while (head)
+	{
+		prep = head->next;
+		free(prep->str);
+		free(head);
+		head = prep;
+	}
+}
